4f7a95a4b169f99cd2d6d3008f5468ff6fa057f1
Kirsten S <kirsten@Kirstens-MacBook-Air.local>
2020-07-12 19:33:03 -0400

    created components folder and Login.js, made simple login form

---
+++ friends/src/components/Login.js
@@ -0,0 +1,40 @@
+import React, {useState} from 'react';
+
+const Login = () => {
+    const [formState, setFormState] = useState({
+        username: "",
+        password: ""
+    })
+
+    const handleChanges = e => {
+        setFormState({ ...formState, [e.target.name]: e.target.value })
+    };
+
+    return (
+        <>
+            <form>
+                <label htmlFor="username">Username:</label>
+                <input
+                    type="text"
+                    name="username"
+                    id="username"
+                    placeholder="Enter username..."
+                    value={formState.username}
+                    onChange={handleChanges}
+                />
+                <label htmlFor="password">Password:</label>
+                <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    placeholder="Enter password..."
+                    value={formState.password}
+                    onChange={handleChanges}
+                />
+                <button>Login</button>
+            </form>
+        </>
+    )
+}
+
+export default Login;
