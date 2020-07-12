00435d6b3b84bf69fe43470761135ae919ca5947
Kirsten S <kirsten@Kirstens-MacBook-Air.local>
2020-07-12 19:41:31 -0400

    wrapped App in Router, added Route to login in app

--- friends/src/index.js
+++ friends/src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+//import Router
+import {BrowserRouter as Router} from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+//wrap App in Router
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
