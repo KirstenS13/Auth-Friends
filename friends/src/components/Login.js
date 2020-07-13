fa832dcacf5a4608c754ec0fb924b2b55e8886c4
Kirsten S <kirsten@Kirstens-MacBook-Air.local>
2020-07-12 21:42:19 -0400

    added FriendsList.js, Friend.js, FriendForm.js, PrivateRoute.js components, set Login.js formState.credentials to the correct username and password, set the response from the post request to localStorage under token

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,20 +1,37 @@
 import React, {useState} from 'react';
+import axios from 'axios';
 
 const Login = () => {
     const [formState, setFormState] = useState({
         credentials: {
-            username: "",
-            password: ""
-        }
+            username: "Lambda School",
+            password: "i<3Lambd4"
+        },
+        isLoading: false
     })
 
     const handleChanges = e => {
         setFormState({ ...formState, [e.target.name]: e.target.value })
     };
 
+    const handleLogin = e => {
+        e.preventDefault();
+        setFormState({ ...formState, isLoading: true });
+        axios
+            .post("http://localhost:5000/api/login", formState.credentials)
+            .then(res => {
+                console.log('res', res);
+                setFormState({ ...formState, isLoading: false});
+                localStorage.setItem("token", res.data.payload);
+            })
+            .catch(err => {
+                console.log('err', err);
+            })
+    }
+
     return (
         <>
-            <form>
+            <form onSubmit={handleLogin}>
                 <label htmlFor="username">Username:</label>
                 <input
                     type="text"
@@ -33,7 +50,7 @@ const Login = () => {
                     value={formState.password}
                     onChange={handleChanges}
                 />
-                <button>Login</button>
+                <button>{formState.isLoading === true ? <p>Loading...</p> : "Login"}</button>
             </form>
         </>
     )
